452062294f2d7665afae3eec83bee3057975d530
Daniel Radu <radu.daniel@gmail.com>
2015-04-06 13:24:26 +0300

    Suggestion
    Added a comment

--- ServoControl/ServoControl.ino
+++ ServoControl/ServoControl.ino
@@ -39,7 +39,7 @@ void setup()
 void loop() {}
 
 //Wire receiver methods------------------------------------------------------------------------------------------------------------------------------------------
-
+//TODO: MVD logic
 String receiverWireString;
 
 void receiveEvent(int howMany)
