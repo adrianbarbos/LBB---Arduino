e473d593cedb427d7c5b0d52b8c7c01c82469c70
adrianbarbos <adrianbarbos@Adrians-MacBook-Pro.local>
2015-04-07 13:00:41 +0300

    LBB-Servo-Temp

--- LBB-Servo-Temp/LBB-Servo-Temp/LBB-Servo-Temp.ino
+++ LBB-Servo-Temp/LBB-Servo-Temp/LBB-Servo-Temp.ino
@@ -1,12 +1,10 @@
-#include <Wire.h>
 #include <Servo.h>
 
-int servoPin = 9; //Servo pin
-int redLedPin = 12; // Led Pin
 int sensorPin = 0;
-Servo servo;
-int wireTransmissionChannel = 0x60;
+int servoPin = 0;
+int redLedPin = 1;
 
+Servo servo;
 
 //String Split Method------------------------------------------------------------------------------------------------------------------------------------------
 
@@ -29,43 +27,23 @@ String getValue(String data, char separator, int index)
 
 //Start Setup------------------------------------------------------------------------------------------------------------------------------------------
 
-void setup()
-{
-  Wire.begin(wireTransmissionChannel);
-  Wire.onReceive(receiveEvent);
-  Wire.onRequest(requestEvent);
-  Serial.begin(9600);
-  servo.attach(servoPin);
+void setup() {
+  Serial.begin(57600);
+  servo.attach(servoPin); //Attach servo to servoPin
   pinMode(redLedPin, OUTPUT);
 }
 
-void loop() {}
-
-//Wire receiver methods------------------------------------------------------------------------------------------------------------------------------------------
-
-String receiverWireString;
-
-void receiveEvent(int howMany)
-{
-  char ch = Wire.read();
-  //Serial.println(ch);
-  constructWireString(ch);
-
+void loop() {
+    if(Serial.available()) {
+        String str = Serial.readString();
+        serialAction(str);
+    }
 }
 
-void constructWireString(char ch) {
-  if (ch != '$') {
-    receiverWireString += String(ch);
-  } else {
-    Serial.println(receiverWireString);
-    wireAction();
-    receiverWireString = "";
-  }
-}
-
-void wireAction() {
-  String option = getValue(receiverWireString, ';', 0);
-  String angleString = getValue(receiverWireString, ';', 1);
+void serialAction(String str) {
+  
+  String option = getValue(str, ';', 0);
+  String angleString = getValue(str, ';', 1);
   int angle = angleString.toInt();
   Serial.println("Option " + option);
   Serial.println("Angle " + angleString);
@@ -73,18 +51,11 @@ void wireAction() {
   if (option == "1") {
     writeServo(angle);
     digitalWrite(redLedPin, HIGH);
-    //Wire.beginTransmission(50);
-    //Wire.write(0);
-    //Wire.endTransmission();
   }
   if (option == "2") {
     digitalWrite(redLedPin, LOW);
-    //Serial.print("Current Servo Position: ");
-    //Serial.println(readServo());
-//    Wire.beginTransmission(wireTransmissionChannel);
-   //Wire.write('1');
-//    Wire.endTransmission();
-    
+    Serial.println("Current Servo Position: ");
+    Serial.print(readServo());    
   }
   if (option == "3") {
     
@@ -95,7 +66,7 @@ void wireAction() {
        voltage /= 1024.0; 
        
        // print out the voltage
-       Serial.print(voltage); Serial.println(" volts");
+       //Serial.print(voltage); Serial.println(" volts");
        
        // now print out the temperature
        float temperatureC = (voltage - 0.5) * 100 ;  //converting from 10 mv per degree wit 500 mV offset
@@ -103,17 +74,13 @@ void wireAction() {
        Serial.print(temperatureC); Serial.println(" degrees C");
        
        // now convert to Fahrenheit
-       float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
-       Serial.print(temperatureF); Serial.println(" degrees F");
+       //float temperatureF = (temperatureC * 9.0 / 5.0) + 32.0;
+       //Serial.print(temperatureF); Serial.println(" degrees F");
        
        delay(1000); 
     
   }
-}
-
-//Wire request methos
-void requestEvent() {
-  Wire.write("hello");
+  
 }
 
 //Servo methods------------------------------------------------------------------------------------------------------------------------------------------
