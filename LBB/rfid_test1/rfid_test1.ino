ab70d4aeb2fbd68e77900014ea4328cd25d39276
Daniel Radu <radu.daniel@gmail.com>
2015-04-10 21:31:35 +0200

    rfid test
    added the rfid test

---
+++ LBB/rfid_test1/rfid_test1.ino
@@ -0,0 +1,38 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial RFID(A0, A1); // RX and TX
+
+int i;
+
+void setup()
+
+{
+  RFID.begin(9600); // start serial to RFID read
+  Serial.begin(9600);  // start serial to PC
+
+  Serial.print("HelloWorld");
+
+}
+
+
+
+void loop()
+
+{
+
+  if (RFID.available() > 0)
+
+  {
+
+    //Serial.print(“HelloWorld3″);
+
+    i = RFID.read();
+
+    Serial.print(i, DEC);
+
+    Serial.print(" ");
+
+  }
+
+}
+
